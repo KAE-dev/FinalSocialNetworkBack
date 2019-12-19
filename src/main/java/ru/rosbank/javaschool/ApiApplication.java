5224f059473ad98294cd8382b1c16c5c0501a034
KAE-dev <kenobidark@gmail.com>
2019-12-19 23:05:32 +0300

    added travis

--- src/main/java/ru/rosbank/javaschool/ApiApplication.java
+++ src/main/java/ru/rosbank/javaschool/ApiApplication.java
@@ -28,13 +28,13 @@ public class ApiApplication {
       UserSaveRequestDto rosbankSave = new UserSaveRequestDto(0, "Росбанк", "rosbank", "rosbank", "89999876543");
       UserEntity rosbank = service.save(rosbankSave);
       repository.saveAll(List.of(
-              new PostEntity(0, petya, "Всех приветствую", null, false, 2),
-              new PostEntity(0, petya, "Как дела?", null, false, 12),
               new PostEntity(0, petya, "Новые звездные воины так-себе", null, false, 22),
-              new PostEntity(0, rosbank,  "Сегодня день сдачи", null, false, 122),
-              new PostEntity(0, rosbank,  "В офисе Росбанка", null, false, 542),
               new PostEntity(0, rosbank,  "Хочу кофе и спать", null, false, 3),
-              new PostEntity(0, rosbank, "Хочу кофе", null, false, 653)
+              new PostEntity(0, rosbank, "Хочу кофе", null, false, 653),
+              new PostEntity(0, rosbank,  "В офисе Росбанка", null, false, 542),
+              new PostEntity(0, rosbank,  "Сегодня день сдачи", null, false, 122),
+              new PostEntity(0, petya, "Как дела?", null, false, 12),
+              new PostEntity(0, petya, "Всех приветствую", null, false, 2)
       ));
     };
   }
